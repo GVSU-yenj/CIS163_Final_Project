f225a4497cf4c859accca6b0c19cc523a6552d3b
GVSU-yenj <yenj@mail.gvsu.edu>
2017-04-02 12:57:16 -0400

    Update FCGUI.java

--- FCGUI.java
+++ FCGUI.java
@@ -2,4 +2,22 @@ package FoodCourt;
 
 public class FCGUI {
 
+  /***************
+  *JMONEY Addition
+  *****************/
+  public FCGUI(){
+private JPanel deezButtons = new JPanel;
+deezButtons.setLayout(new GridLayout(0,2));
+
+  //Jmoney: might have to add actionListeners for my buttons.
+private JButton starter = new JButton("Start da Sim!");
+private JButton quitter = new JButton("Quit da Sim!");
+ 
+ deezButtons.add(starter);
+ deezButtons.add(quitter);
+ }
+/**************
+*END JMONEY ADDITION
+**************/
+  
 }
