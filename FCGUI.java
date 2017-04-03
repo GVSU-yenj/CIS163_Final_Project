948607eead8f8bf395aad77386245c6bfd1ce1cd
GVSU-yenj <yenj@mail.gvsu.edu>
2017-04-03 19:10:02 -0400

    Revert "Cousinojake"

--- FCGUI.java
+++ FCGUI.java
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-//package FoodCourt;
-package gui;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class FCGUI extends JFrame{
-	
-	public FCGUI(){
-	
-	
-	
-	JPanel outInfoPanel = new JPanel();	
-	outInfoPanel.setLayout(new GridLayout(0,2));
-    
-    JLabel through = new JLabel("Through put");
-    JLabel avgTime = new JLabel("Average time for a person from start to finish");
-    JLabel numberPeople = new JLabel("Number of people left in line");
-    JLabel maxQ = new JLabel("Max Q length cashier line");
-    
-    JLabel throughInfo = new JLabel(" ");
-    JLabel avgTimeInfo = new JLabel(" ");
-    JLabel numberPeopleInfo = new JLabel(" ");
-    JLabel maxQInfo = new JLabel(" ");
-    
-    outInfoPanel.add(through);
-    outInfoPanel.add(throughInfo);
-    outInfoPanel.add(avgTime);
-    outInfoPanel.add(avgTimeInfo);
-    outInfoPanel.add(numberPeople);
-    outInfoPanel.add(numberPeopleInfo);
-    outInfoPanel.add(maxQ);
-    outInfoPanel.add(maxQInfo);
-    
-    mainLayout.add(outInfoPanel, BorderLayout.SOUTH);
-	}
-
-}
-=======
 package FoodCourt;
 
 import java.awt.*;
@@ -99,4 +55,3 @@ public class FCGUI extends JFrame implements KeyEventDispatcher, ActionListener 
 	}
   
 }
->>>>>>> master
