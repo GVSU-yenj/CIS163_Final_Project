c5ee76e877b62179e2258b3a923aefd1c5fe9434
Tim Irish <irisht@mail.gvsu.edu>
2017-04-14 21:57:16 -0400

    Update Eatery.java

--- W17Project4Student/W17Project4Student/Eatery.java
+++ W17Project4Student/W17Project4Student/Eatery.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 import FoodCourt.FCGUI;
 /**
- * @author   Roger Ferguson
+ * @author Tim Irish, Jon Yen, Jake Cousino
  */
 public class Eatery implements ClockListener {
 	private ArrayList<Person> Q = new ArrayList<Person>();
@@ -15,7 +15,10 @@ public class Eatery implements ClockListener {
 	private CIS163Q<Person> Que = new CIS163Q<Person>();
 	private double val;
 	
-	
+	/**
+	* adds a person to the eatery line and then sets the max line length
+	* @param person a new person object 
+	*/
 	public void add (Person person)
 	{
 		//System.out.println("This is the value passed into this function fro GUI: " + this.val);
@@ -59,30 +62,59 @@ public class Eatery implements ClockListener {
 	}
 	
 	
-	
+	/**
+	* If the number of flags is equal to one a person is removed
+	*/
 	public void getFlag(){
 		if(person.getFlag() ==1){
 			person = Q.remove(0);
 		}
 	}
 	
+	/**
+	* returns the amount of people left in the eatery line
+	* @return   size of the Q 
+	*/
 	public int getLeft() {
 		return Q.size();
 	}
 	
+	/**
+	* returns the max amount of people that were 
+	* on the eatery line.
+	* @return  max length of the Q. 
+	*/
 	public int getMaxQlength() {
 		return maxQlength;
 	}
+	
+	/**
+	* return the amount of people that got through the eatery line
+	* @return currentCompleted  
+	*/
 	public int getCurrentCompleted(){
 		return currentCompleted;
 	}
-
+	
+	/**
+	* return the amount of people that got through the eatery line minus 1
+	* @return completed 
+	*/
 	public int getThroughPut() {
 		return completed;
 	}
+	
+	/**
+	* sets a person object to temPer
+	*/
 	public void setPerson(Person p){
 		tempPer = p;
 	}
+	
+	/**
+	* returns temPer
+	* @return temPer
+	*/
 	public Person personGetter(){
 		return tempPer;
 	}
